88dd3e266e1844b1dea49fe17088e3970f6d5308
Rina <taigynova@gmail.com>
2025-02-15 18:46:23 +0300

    add frontend and backend

---
+++ backend/src/main/java/com/example/VetTies/VetTiesApplication.java
@@ -0,0 +1,13 @@
+package com.example.VetTies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VetTiesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VetTiesApplication.class, args);
+	}
+
+}
